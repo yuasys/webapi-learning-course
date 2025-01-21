ea5c22827f12b5c1c69bd5ec4168387461924830
kentny <21260198+kentny@users.noreply.github.com>
2025-01-22 05:29:22 +0900

    Update the port to be used.

--- cache/app.py
+++ cache/app.py
@@ -41,4 +41,4 @@ def with_stale_while_revalidate():
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
